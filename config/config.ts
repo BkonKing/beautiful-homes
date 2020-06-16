1b31c713e16d890774519fd748a1e0b07fb4e633
hongbingkun <hongbingkun@bosssoft.com.cn>
2020-06-16 09:50:48 +0800

    feat(basic):去除国际化功能

--- config/config.ts
+++ config/config.ts
@@ -16,20 +16,13 @@ export default defineConfig({
   dva: {
     hmr: true,
   },
-  locale: {
-    // default zh-CN
-    default: 'zh-CN',
-    // default true, when it is true, will use `navigator.language` overwrite default
-    antd: true,
-    baseNavigator: true,
-  },
   dynamicImport: {
     loading: '@/components/PageLoading/index',
   },
   targets: {
     ie: 11,
   },
-  // umi routes: https://umijs.org/docs/routing 
+  // umi routes: https://umijs.org/docs/routing
   routes,
   // Theme for antd: https://ant.design/docs/react/customize-theme-cn
   theme: {
--- src/layouts/BasicLayout.tsx
+++ src/layouts/BasicLayout.tsx
@@ -10,17 +10,14 @@ import ProLayout, {
   DefaultFooter,
 } from '@ant-design/pro-layout';
 import React, { useEffect } from 'react';
-import { Link, useIntl, connect, Dispatch } from 'umi';
+import { Link, connect, Dispatch } from 'umi';
 import { GithubOutlined } from '@ant-design/icons';
 import { Result, Button } from 'antd';
 import Authorized from '@/utils/Authorized';
 import RightContent from '@/components/GlobalHeader/RightContent';
 import { ConnectState } from '@/models/connect';
 import { getAuthorityFromRouter } from '@/utils/utils';
-import logo from '../assets/logo.svg';
-
-// const [menuData, setMenuData] = useState([]);
-
+import logo from '../assets/logo.svg'; // const [menuData, setMenuData] = useState([]);
 // useEffect(() => {
 //   // 这里是一个演示用法
 //   // 真实项目中建议使用 dva dispatch 或者 umi-request
@@ -43,8 +40,6 @@ const noMatch = (
     }
   />
 );
-
-
 export interface BasicLayoutProps extends ProLayoutProps {
   breadcrumbNameMap: {
     [path: string]: MenuDataItem;
@@ -135,12 +130,9 @@ const BasicLayout: React.FC<BasicLayoutProps> = (props) => {
   const authorized = getAuthorityFromRouter(props.route.routes, location.pathname || '/') || {
     authority: undefined,
   };
-  const { formatMessage } = useIntl();
-
   return (
     <ProLayout
       logo={logo}
-      formatMessage={formatMessage}
       menuHeaderRender={(logoDom, titleDom) => (
         <Link to="/">
           {logoDom}
@@ -152,12 +144,13 @@ const BasicLayout: React.FC<BasicLayoutProps> = (props) => {
         if (menuItemProps.isUrl || !menuItemProps.path) {
           return defaultDom;
         }
+
         return <Link to={menuItemProps.path}>{defaultDom}</Link>;
       }}
       breadcrumbRender={(routers = []) => [
         {
           path: '/',
-          breadcrumbName: formatMessage({ id: 'menu.home' }),
+          breadcrumbName: '首页',
         },
         ...routers,
       ]}
